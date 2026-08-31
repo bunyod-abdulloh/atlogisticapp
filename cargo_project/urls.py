d0b08c94e0e87337af5f5014fa8ac2ec7bbb1ff6
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2026-08-31 14:41:54 +0500

    Commited

--- cargo_project/urls.py
+++ cargo_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("user/", include("tracking.urls")),
     path("user/api/", include("shipments.urls")),
 
 ]
